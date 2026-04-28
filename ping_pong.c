000dcf7a201acc69b884b70fb9ea77f48234ac48
Atamawaru <karolis.markevicius4@gmail.com>
2026-04-28 20:38:41 +0300

    Render the background in green for now

--- ping_pong.c
+++ ping_pong.c
@@ -1,5 +1,6 @@
 #include <SDL3/SDL_events.h>
 #include <SDL3/SDL_init.h>
+#include <SDL3/SDL_oldnames.h>
 #include <SDL3/SDL_render.h>
 #include <SDL3/SDL_video.h>
 #include <SDL3/SDL.h>
@@ -25,6 +26,7 @@ int main(int argc, char *argv[]){
                 quit=true;
             }
         }
+        SDL_SetRenderDrawColorFloat(gRenderer, 0, 255, 0, 20);
         SDL_RenderClear(gRenderer);
         SDL_RenderPresent(gRenderer);
     }
